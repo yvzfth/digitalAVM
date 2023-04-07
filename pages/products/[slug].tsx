49b83ffd1dbcb928fa34b2ef69231262721e594a
Fatih Yavuz <yvzfth@yandex.com>
2023-04-07 22:32:07 +0300

    detail page first commit

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,10 +1,14 @@
+import products from '@/utils/products';
 import { useRouter } from 'next/router';
 import React from 'react';
 
 const ProductDetail = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  const product = products.find((product) => product.title === slug);
+
+  // -------- CHANGE BELOW ------------------
+  return <div>{JSON.stringify(product)}</div>;
 };
 
 export default ProductDetail;
